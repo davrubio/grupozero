71c5ae3403ce57a18b9eed32e7615ec123d527ed
David Rubio <dav.rubio@duocuc.cl>
2023-05-06 22:17:47 -0400

    update function

--- redirection.js
+++ redirection.js
@@ -1,5 +1,14 @@
 
-window.onload = () => {
+/* window.onload = () => {
   const seccionDestino = document.getElementById();
   seccionDestino.scrollIntoView({ behavior: 'smooth' });
-}
+} */
+
+
+redireccionar = (id) => {
+  const seccionDestino = id;
+  /* seccionDestino.id.scrollIntoView({ behavior: 'smooth' }); */
+  /* console.log(seccionDestino);
+  console.log(seccionDestino.id); */
+  window.location.href = 'galeria.html#'+seccionDestino;
+};
