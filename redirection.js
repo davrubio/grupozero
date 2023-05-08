73d512a264899d8e272a503490c7fd56451b1562
David Rubio <dav.rubio@duocuc.cl>
2023-05-07 23:31:33 -0400

    update login

--- redirection.js
+++ redirection.js
@@ -1,10 +1,4 @@
 
-/* window.onload = () => {
-  const seccionDestino = document.getElementById();
-  seccionDestino.scrollIntoView({ behavior: 'smooth' });
-} */
-
-
 redireccionar = (id) => {
   const seccionDestino = id;
   /* seccionDestino.id.scrollIntoView({ behavior: 'smooth' }); */
